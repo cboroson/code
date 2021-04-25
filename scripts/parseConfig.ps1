6e849ee1868676c594d642544df36f81f58b7f9e
cboroson <cboroson@gmail.com>
2021-04-25 09:49:26 -0400

    Create parseConfig.ps1

---
+++ scripts/parseConfig.ps1
@@ -0,0 +1,22 @@
+# Reads json configuration file and creates environment variables for each entry
+
+$config = (get-content ~/Source/configuration/admins09.json | convertFrom-json)
+foreach ($Line in $config.PSObject.Properties) {
+
+  $EnvVarName = "VOD_$($Line.Name)"
+  $EnvVarValue = $Line.Value
+
+  # Display output for happiness
+  write-output "Adding environment value name $EnvVarName with a value of $EnvVarValue"
+
+  # Test if environment variable already exists
+  if (Test-Path Env:$EnvVarName) {
+    # Update existing environment variable
+    Set-Item -path Env:$EnvVarName -Value $EnvVarValue | out-null
+  }
+  else {
+    # Create new environment variable
+    New-Item -Path Env:$EnvVarName -Value $EnvVarValue | out-null
+  }
+
+}
