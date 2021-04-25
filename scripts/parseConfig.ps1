8b40a96de5a89e58615d52900e814360c7176095
cboroson <cboroson@gmail.com>
2021-04-25 10:21:51 -0400

    Update parseConfig.ps1

--- scripts/parseConfig.ps1
+++ scripts/parseConfig.ps1
@@ -10,23 +10,8 @@ foreach ($Line in $config.PSObject.Properties) {
   $EnvVarName = "VOD_$($Line.Name)"
   $EnvVarValue = $Line.Value
 
-  # Test if environment variable already exists
-  if (Test-Path Env:$EnvVarName) {
-    # Update existing environment variable
-    write-output "Setting existing environment variable $EnvVarName with a value of $EnvVarValue"
-    #Set-Item -path Env:$EnvVarName -Value $EnvVarValue #| out-null
-    Write-output "$EnvVarName=$EnvVarValue" | out-file $ENV:GITHUB_ENV -append
-  }
-  else {
-    # Create new environment variable
-    write-output "Adding new environment variable $EnvVarName with a value of $EnvVarValue"
-    #New-Item -Path Env:$EnvVarName -Value $EnvVarValue #| out-null
-    Write-output "$EnvVarName=$EnvVarValue" | out-file $ENV:GITHUB_ENV -append
-
-  }
+  # Create new environment variable
+  write-output "Adding new environment variable $EnvVarName with a value of $EnvVarValue"
+  Write-output "$EnvVarName=$EnvVarValue" | out-file $ENV:GITHUB_ENV -append
 
 }
-
-#dir env:
-Write-Output "GitHub_ENV = $ENV:GITHUB_ENV"
-cat $ENV:GITHUB_ENV
