f83616bbac5208f20b8cccd818873331b2b1dc9f
cboroson <cboroson@gmail.com>
2021-04-25 09:58:11 -0400

    Update parseConfig.ps1

--- scripts/parseConfig.ps1
+++ scripts/parseConfig.ps1
@@ -10,17 +10,17 @@ foreach ($Line in $config.PSObject.Properties) {
   $EnvVarName = "VOD_$($Line.Name)"
   $EnvVarValue = $Line.Value
 
-  # Display output for happiness
-  write-output "Adding environment value name $EnvVarName with a value of $EnvVarValue"
-
   # Test if environment variable already exists
   if (Test-Path Env:$EnvVarName) {
     # Update existing environment variable
-    Set-Item -path Env:$EnvVarName -Value $EnvVarValue | out-null
+    write-output "Setting existing environment variable $EnvVarName with a value of $EnvVarValue"
+    Set-Item -path Env:$EnvVarName -Value $EnvVarValue #| out-null
   }
   else {
     # Create new environment variable
-    New-Item -Path Env:$EnvVarName -Value $EnvVarValue | out-null
+    write-output "Adding new environment variable $EnvVarName with a value of $EnvVarValue"
+    New-Item -Path Env:$EnvVarName -Value $EnvVarValue #| out-null
   }
 
+  set
 }
